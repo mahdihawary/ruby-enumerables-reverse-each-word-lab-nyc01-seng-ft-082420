9ae9e8978d812c0b534a25319b1f414c36cf7026
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-16 05:57:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def reverse_each_word(string)
 string_array = string.split(" ")
-string_array.collect {|string|string.reverse}
+string_array.collect {|string|string.reverse}.join(" ")
 binding.pry
 end
